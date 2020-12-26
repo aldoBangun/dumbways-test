a092ce8e9bfef952cb4ae25cb48194ee04d6fa80
aldoBangun <punk.aldo68@gmail.com>
2020-12-26 16:21:51 +0800

    add register and login to user model

--- models/users.js
+++ models/users.js
@@ -1,5 +1,26 @@
 const sql = require('../config/db.config')
 
 module.exports = {
-   
+   register: (newUser, callback) => {
+      sql.query('INSERT INTO users_tb SET ?', newUser, (err, result) => {
+         if (err) {
+            callback(err)
+         } else {
+            callback(null, result)
+         }
+      })
+   },
+   login: (user, callback) => {
+      sql.query(
+         'SELECT * FROM users_tb WHERE email=? AND password=?', 
+         [user.email, user.password],
+         (err, results) => {
+            if (err) {
+               callback(err)
+            } else {
+               callback(null, results[0])
+            }
+         }
+      )      
+   }
 }
