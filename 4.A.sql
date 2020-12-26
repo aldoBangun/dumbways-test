34be3d393cae2862b82a2dd217358805992ae9e4
aldoBangun <punk.aldo68@gmail.com>
2020-12-26 12:56:25 +0800

    menyelesaikan soal nomor 4.A (membuat sql query)

---
+++ 4.A.sql
@@ -0,0 +1,16 @@
+-- Tampilkan seluruh data dari table user beserta Contentnya 
+SELECT * FROM post_tb
+JOIN users_tb
+ON post_tb.id_user = users_tb.id;
+
+
+-- Insert seluruh data dengan table yang terkait
+INSERT INTO users_tb (name, photo, email, password)
+VALUES ('Iwan Tafui', 'iwan.jpg', 'iwan@test.com', 'secret');
+
+INSERT INTO post_tb (content, image, id_user)
+VALUES ('Wadoh lupa beli camilan', 'test-image5.jpg', 1);
+
+
+-- Tampilkan seluruh data content berdasarkan user tertentu
+SELECT * FROM post_tb WHERE id_user=1;
